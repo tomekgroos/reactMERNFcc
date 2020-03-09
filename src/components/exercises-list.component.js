8372a05b4e5972d87844cb528a0b6ea55003c031
Your Name <you@example.com>
2020-03-09 23:06:49 +0100

    end of code day 09.03.2020

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import axios from 'axios';
 
+const Exercise = props => {
+    <tr>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.description}</td>
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0,10)}</td>
+        <td>
+            <Link to={"/edit" + props.exercise._id}>edit</Link>
+            | <a href='#' onClick={() => {props.deleteExercise(props.exercise._id)}}>delete</a>
+        </td>
+    </tr>
+} // do poprawy?
+
 class ExercisesList extends Component {
     constructor(props){
         super(props);
@@ -22,10 +35,40 @@ class ExercisesList extends Component {
             })
         }
 
+        deleteExercise(id){
+            axios.delete('http://localhost:4000/exercises' + id)
+            .then(res => console.log(res.data));
+            this.setState({
+                exercises: this.state.exercises.filter(el => el._id !== id)
+            })
+        }
+
+        exerciseList() {
+            return this.state.exercises.map(currentexercise =>{
+                return <Exercise exercise={currentexercise} 
+                deleteExercise={this.deleteExercise}
+                key={currentexercise._id}/>
+            })
+        }
+
     render() {
         return (
             <div>
                 <h1>This is the list</h1>
+                <table className="table">
+                <thead className="thead-light">
+                <tr>
+                    <th>Username</th>
+                    <th>Description</th>
+                    <th>Duration</th>
+                    <th>Date</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {this.exerciseList()}
+                </tbody>
+                </table>
             </div>
         )
     }
